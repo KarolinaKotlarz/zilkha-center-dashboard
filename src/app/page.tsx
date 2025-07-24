d1659f1f8a540a6e610c1e1bf6545dc109328b98
Karolina Kotlarz <kotlarzkarolina16@gmail.com>
2025-07-24 14:16:16 -0400

    improved sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,67 @@
+"use client"
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-(family-name:--font-geist-sans)">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-(family-name:--font-geist-mono)">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/5 dark:bg-white/6 px-1 py-0.5 rounded-sm font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import React, { useState } from "react"
+import { Card } from '@/components/Card';
+import { Checkbox } from "@/components/Checkbox"
+import { Label } from '@/components/Label';
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/8 dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+export const ExplanationCard = () => (
+  <Card>
+    <h3 className="font-semibold text-gray-900 dark:text-gray-50">
+      Energy Consumption in the Zilkha Center
+    </h3>
+    <p className="mt-2 text-sm leading-6 text-gray-900 dark:text-gray-50">
+      This dashboard lets you see the energy consumption 
+      from the various devices in the Zilkha Center. You 
+      can toggle the time range as well as set which devices 
+      are visible on the graph. Use the checkboxes on the side 
+      to set which items are visible. Click the arrows in the 
+      top-right corner of the visualization to expand it to a full screen.
+    </p>
+  </Card> 
+);
+
+export const CheckboxWithLabel = ({label, onChecked} : {label: string, onChecked: any }) => (
+  <div className="flex items-center justify-start gap-2">
+    <Checkbox id="r1" onCheckedChange={onChecked} defaultChecked={true}/>
+    <Label htmlFor="r1">{label}</Label>
+  </div>
+);
+
+export default function Home() {
+  const [showChart, setShowChart] = useState(true);
+  const [showExplanation, setShowExplanation] = useState(true);
+
+  return (
+    <div className="mx-auto max-w-screen-2xl">
+      <div className="lg:w-64 lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:flex-col">
+      <aside className="flex grow flex-col gap-y-4 overflow-y-auto whitespace-nowrap px-3 py-4">
+          <span className="p-5 text-sx font-semibold text-gray-900">Energy Dashboard</span>
+        <div className="ms-8 flex flex-col gap-5">
+          <CheckboxWithLabel label="Explanation" onChecked={() => setShowExplanation(!showExplanation)}/>
+          <CheckboxWithLabel label="Graph" onChecked={setShowChart}/>
+      </div>
+      </aside>
+      </div>
+      <main className="lg:pl-64 lg:bg-blue-50 lg:py-3 lg:pr-3 lg:dark:bg-gray-950">
+        <div className="bg-white p-4 sm:p-6 lg:rounded-lg lg:border lg:border-gray-200 dark:bg-gray-925 lg:dark:border-gray-900">   
+          <section className="sticky top-16 z-50 -my-6 flex flex-col gap-6 bg-white py-6 md:flex-row md:flex-wrap md:items-center md:justify-between lg:top-0 dark:bg-gray-925 border-b border-gray-200 transition-all dark:border-gray-900">
+            Filters
+          </section>
+          <section className="my-8">
+            {showExplanation && <ExplanationCard/>}
+            {/* {showExplanation && <ExplanationCard/>}
+            {showChart && <Suspense fallback={<AreaChartFallback/>}>
+            <ServerAreaChart/>
+            </Suspense>}
+            <Card className="min-h-64">
+              Visualization
+            </Card> */}
+            Section
+          </section>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
