07f15701768f55ea2a68c7f0eaaf8c124af701f3
Karolina Kotlarz <kotlarzkarolina16@gmail.com>
2025-07-25 10:51:17 -0400

    added areaChart component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import React, { useState } from "react"
 import { Card } from '@/components/Card';
 import { Checkbox } from "@/components/Checkbox"
 import { Label } from '@/components/Label';
+import { AreaChartHero } from "@/components/custom/TotalEnergyChartPlaceholder";
 
 export const ExplanationCard = () => (
   <Card>
@@ -35,22 +36,21 @@ export default function Home() {
 
   return (
     <div className="mx-auto max-w-screen-2xl">
-      <div className="lg:w-64 lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:flex-col">
-      <aside className="flex grow flex-col gap-y-4 overflow-y-auto whitespace-nowrap px-3 py-4">
+      <aside className="lg:w-64 lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:flex-col flex grow flex-col gap-y-4 overflow-y-auto whitespace-nowrap px-3 py-4">
           <span className="p-5 text-sx font-semibold text-gray-900">Energy Dashboard</span>
         <div className="ms-8 flex flex-col gap-5">
           <CheckboxWithLabel label="Explanation" onChecked={() => setShowExplanation(!showExplanation)}/>
           <CheckboxWithLabel label="Graph" onChecked={setShowChart}/>
       </div>
       </aside>
-      </div>
       <main className="lg:pl-64 lg:bg-blue-50 lg:py-3 lg:pr-3 lg:dark:bg-gray-950">
         <div className="bg-white p-4 sm:p-6 lg:rounded-lg lg:border lg:border-gray-200 dark:bg-gray-925 lg:dark:border-gray-900">   
           <section className="sticky top-16 z-50 -my-6 flex flex-col gap-6 bg-white py-6 md:flex-row md:flex-wrap md:items-center md:justify-between lg:top-0 dark:bg-gray-925 border-b border-gray-200 transition-all dark:border-gray-900">
             Filters
           </section>
           <section className="my-8">
-            {showExplanation && <ExplanationCard/>}
+            {/* {showExplanation && <ExplanationCard/>} */}
+            <AreaChartHero/>
             {/* {showExplanation && <ExplanationCard/>}
             {showChart && <Suspense fallback={<AreaChartFallback/>}>
             <ServerAreaChart/>
@@ -58,7 +58,7 @@ export default function Home() {
             <Card className="min-h-64">
               Visualization
             </Card> */}
-            Section
+            {/* Section */}
           </section>
         </div>
       </main>
