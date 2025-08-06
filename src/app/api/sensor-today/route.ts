e5a318979237effeae838227673edb6276fd8445
Karolina Kotlarz <kotlarzkarolina16@gmail.com>
2025-08-06 14:48:15 -0400

    commented sensor-today route.ts

--- src/app/api/sensor-today/route.ts
+++ src/app/api/sensor-today/route.ts
@@ -2,29 +2,28 @@ import { getAuthToken, getDeviceData } from "@/app/actions";
 
 export async function GET(req: Request) {
     const { searchParams } = new URL(req.url);
+    // If the ID is empty, return an error
+    // Otherwise we assume the ID is correct
     const id = searchParams.get('id');
     if (id === null) {
-        return new Response('Invalid URL ID', { status: 400 });
+        return new Response('Provide URL ID', { status: 400 });
     }
     
     const JWT = await getAuthToken(id);
     const data = await getDeviceData(id, JWT);
 
+    // We need to map device names to their values to make the chart visualization easier
     var dictionary = data.ranges.map(function(row) {
+        // We map names to values, then add a time to each object
         const r = row.power.reduce(function(result: { [x: string]: any; }, field: any, index: string | number) {
                 result[data.names[index]] = field;
                 return result;
             }, {});
-            r['time'] = row.time
+            r['time'] = row.time;
             return r;
     });
 
-    console.log(dictionary);
-
-    const result = { names: data.names, data: dictionary}
-
-    console.log('res');
-    console.log(result)
+    const result = { names: data.names, data: dictionary };
 
     return new Response(JSON.stringify(result), {
         status: 200,
