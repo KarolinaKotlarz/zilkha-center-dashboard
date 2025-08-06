39cf3f416257e29b5ddfbcfea7036aef546abf8e
Karolina Kotlarz <kotlarzkarolina16@gmail.com>
2025-08-06 14:44:52 -0400

    cleaned up sensor-today route.ts

--- src/app/api/sensor-today/route.ts
+++ src/app/api/sensor-today/route.ts
@@ -1,5 +1,4 @@
 import { getAuthToken, getDeviceData } from "@/app/actions";
-import { sensors } from "@/app/data/sensors"
 
 export async function GET(req: Request) {
     const { searchParams } = new URL(req.url);
@@ -10,39 +9,9 @@ export async function GET(req: Request) {
     
     const JWT = await getAuthToken(id);
     const data = await getDeviceData(id, JWT);
-    // console.log(data);
-
-    // const res = data.ranges.map((val) => {
-    //     return val.power.reduce((accumulator, field, index) => {
-    //         accumulator[index] = field.toString();
-    //         console.log(field);
-    //         console.log(data.names[index]);
-    //         return '';
-    //     });
-    // })
-
-    // var dic = data.ranges.map(function(row) {
-    // return row.power.reduce(function(result, field, index) {
-    //         result[data.names[index]] = field;
-    //         return result;
-    //     }, {});
-    // });
-
-    // console.log('dictionary ' + dic.toString())
-    // console.log(Object.entries(dic))
-    // const entries = new Map(
-    //         Object.entries(dic)
-    //     );
-    // const obj = Object.fromEntries(entries)
-
-    // console.log(obj)
-
-
-    // const map = Object.fromEntries((data.names.map((val, index) => [val, data.ranges[0].power[index]])));
-    // console.log(map)
 
     var dictionary = data.ranges.map(function(row) {
-        const r = row.power.reduce(function(result, field, index) {
+        const r = row.power.reduce(function(result: { [x: string]: any; }, field: any, index: string | number) {
                 result[data.names[index]] = field;
                 return result;
             }, {});
